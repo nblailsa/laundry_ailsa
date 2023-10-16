fdf769986c87b1c4515a756f7b4bc3082f25f919
nblailsa <141145137+nblailsa@users.noreply.github.com>
2023-10-16 12:16:25 +0700

    database
    database laundry

---
+++ laundry_ukk1.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Okt 2023 pada 06.59
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laundry_ukk1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_member`
+--
+
+CREATE TABLE `tb_member` (
+  `id_member` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `tlp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_outlet`
+--
+
+CREATE TABLE `tb_outlet` (
+  `id_outlet` int(11) NOT NULL,
+  `nama` varchar(15) NOT NULL,
+  `alamat` text NOT NULL,
+  `tlp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_paket`
+--
+
+CREATE TABLE `tb_paket` (
+  `id_paket` int(11) NOT NULL,
+  `id_outlet` int(11) NOT NULL,
+  `jenis` enum('kiloan','selimut','bed cover','kaos','lainnya') NOT NULL,
+  `nama_paket` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_outlet` int(11) NOT NULL,
+  `kode_invoice` varchar(100) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `tgl` datetime NOT NULL,
+  `batas_waktu` datetime NOT NULL,
+  `tgl_bayar` datetime DEFAULT NULL,
+  `biaya_tambahan` int(11) DEFAULT NULL,
+  `diskon` double NOT NULL,
+  `pajak` int(11) NOT NULL,
+  `status` enum('proses','selesai','diambil') NOT NULL,
+  `dibayar` enum('dibayar','belum dibayar') NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_paket` int(11) NOT NULL,
+  `qty` double NOT NULL,
+  `keterangan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `id_outlet` int(11) NOT NULL,
+  `role` enum('admin','kasir','owner') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indeks untuk tabel `tb_outlet`
+--
+ALTER TABLE `tb_outlet`
+  ADD PRIMARY KEY (`id_outlet`);
+
+--
+-- Indeks untuk tabel `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  ADD PRIMARY KEY (`id_paket`),
+  ADD KEY `id_outlet` (`id_outlet`);
+
+--
+-- Indeks untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_outlet` (`id_outlet`,`id_member`,`id_user`,`id_paket`),
+  ADD KEY `id_member` (`id_member`),
+  ADD KEY `id_paket` (`id_paket`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_outlet` (`id_outlet`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_outlet`
+--
+ALTER TABLE `tb_outlet`
+  MODIFY `id_outlet` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  ADD CONSTRAINT `tb_paket_ibfk_1` FOREIGN KEY (`id_outlet`) REFERENCES `tb_outlet` (`id_outlet`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD CONSTRAINT `tb_transaksi_ibfk_1` FOREIGN KEY (`id_member`) REFERENCES `tb_member` (`id_member`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_transaksi_ibfk_2` FOREIGN KEY (`id_paket`) REFERENCES `tb_paket` (`id_paket`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_transaksi_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_transaksi_ibfk_4` FOREIGN KEY (`id_outlet`) REFERENCES `tb_outlet` (`id_outlet`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_outlet`) REFERENCES `tb_outlet` (`id_outlet`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
